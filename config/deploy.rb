22cac53b050e28a8da11d471512be73a4656504e
stawan15 <sutipojr@gmail.com>
2026-05-21 23:22:30 +0700

    chore: update gitignore, standardize deploy configuration, and add linked directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :linked_dirs,  %w[log tmp/pids tmp/cache tmp/sockets storage public/uploads]
 set :keep_releases, 5
 
 # Puma
-set :puma_threads,    [4, 16]
+set :puma_threads,    [ 4, 16 ]
 set :puma_workers,    2
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -31,3 +31,5 @@ SSHKit.config.command_map[:bundle] = "/home/eemevuo/.rbenv/shims/bundle"
 SSHKit.config.command_map[:ruby] = "/home/eemevuo/.rbenv/shims/ruby"
 SSHKit.config.command_map[:gem] = "/home/eemevuo/.rbenv/shims/gem"
 SSHKit.config.command_map[:rake] = "/home/eemevuo/.rbenv/shims/rake"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,9 @@
-server '127.0.0.1', user: 'eemevuo', roles: %w{app db web}, port: 2222
+server '127.0.0.1', user: 'eemevuo', roles: %w[app db web], port: 2222
 
 set :ssh_options, {
   keys: %w[~/.ssh/id_ed25519],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
+
+set :rails_env, :production
